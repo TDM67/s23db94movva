b85f0a274bc70273e114e506e67ac18d77c09bb6
TDM <movvatarundeepchowdary99@gmail.com>
2023-05-03 23:16:42 -0500

    edited files

--- app.js
+++ app.js
@@ -38,41 +38,39 @@ db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
 db.once("open", function(){
 console.log("Connection to DB succeeded")});
 
-
-
 var indexRouter = require('./routes/index');
+var resourceRouter = require('./routes/resource');
 var usersRouter = require('./routes/users');
 var dogRouter = require('./routes/dog');
 var boardRouter = require('./routes/board');
 var selectorRouter = require('./routes/selector');
 var dog = require("./models/dog");
 var app = express();
-var resourceRouter = require('./routes/resource');
+
 async function recreateDB(){
   // Delete everything
-await dog.deleteMany();
+  await dog.deleteMany();
   let instance1 = new
   dog({dog_color:"brown",dog_breed:"German Shepherd",dog_price:2000});
   let instance2 = new
   dog({dog_color:"white",dog_breed:"Labrador Retriever",dog_price:1500});
   let instance3 = new
   dog({dog_color:"black",dog_breed:"Poodle",dog_price:4080});
-  instance1.save().then(doc=>{
-  console.log("First object saved")}
-  ).catch(err=>{
-  console.error(err)});
-  instance2.save().then(doc=>{
-  console.log("Second object saved")}
-  ).catch(err=>{
-  console.error(err)});
-  instance3.save().then(doc=>{
-  console.log("Third object saved")}
-  ).catch(err=>{
-  console.error(err)});
-  }
-  let reseed = true;
-  if (reseed) { recreateDB();
-  }
+  instance1.save( function(err,doc) {
+  if(err) return console.error(err);
+  console.log("First object saved")
+  });
+  instance2.save( function(err,doc) {
+    if(err) return console.error(err);
+    console.log("Second object saved")
+    });
+  instance3.save( function(err,doc) {
+    if(err) return console.error(err);
+    console.log("Third object saved")
+    });
+ }
+ let reseed = true;
+ if (reseed) { recreateDB();}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -82,6 +80,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -90,7 +95,6 @@ app.use('/dog', dogRouter);
 app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/dog.js
+++ routes/dog.js
@@ -7,15 +7,21 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const dog_controlers= require('../controllers/dog');
 var router = express.Router();
+const secured = (req, res, next) => {
+  if(req.user){
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect('/login');
+}
 /* GET dogs */
 router.get('/', dog_controlers.dog_view_all_Page );
 
-router.get('/detail', dog_controlers.dog_view_one_Page);
+router.get('/detail', secured, dog_controlers.dog_view_one_Page);
 
-router.get('/create', dog_controlers.dog_create_Page);
+router.get('/create', secured, dog_controlers.dog_create_Page);
 
-router.get('/update', dog_controlers.dog_update_Page);
+router.get('/update',secured, dog_controlers.dog_update_Page);
 
-router.get('/delete', dog_controlers.dog_delete_Page);
-module.exports = router;
+router.get('/delete', secured, dog_controlers.dog_delete_Page);
 module.exports = router;
